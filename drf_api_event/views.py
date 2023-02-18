4428b97fe0e5c3fe679d0c7d766ca6d3c5291b60
MartinaP87 <bleach1987@hotmail.it>
2023-02-18 11:24:48 +0000

    Add root view

---
+++ drf_api_event/views.py
@@ -0,0 +1,9 @@
+from rest_framework import Response
+from rest_framework.decorators import api_view
+
+
+@api_view()
+def root_route(request):
+    return Response({
+        'message': 'Welcome to my event drf API!'
+    })
